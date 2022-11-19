e7e9ed6837340fa569de78d8951b782cc92b3948
RashmiMaxCoder <86828678+RashmiMaxCoder@users.noreply.github.com>
2022-11-19 14:13:11 +0530

    Update smallest element in an array.cpp

--- DS/smallest element in an array.cpp
+++ DS/smallest element in an array.cpp
@@ -7,30 +7,41 @@
 // max <- array element
 // 
 
-#include<iostream>
-#include<limits.h>
-#include<vector>
+#include<bits/stdc++.h>
 using namespace std;
 
-int fun(vector<int> &vec)
+namespace normal
 {
-   int max_ = INT_MIN; //----- O(1)
+    int fun(vector<int> &vec)
+    {
+    int max_ = INT_MIN; //----- O(1)
 
-   for(int i = 0; i < vec.size(); ++i)
-   {
-        if(max_ < vec[i])
-        max_ = vec[i]; //---- O(n)
-   }
+    for(int i = 0; i < vec.size(); ++i)
+    {
+            if(max_ < vec[i])
+            max_ = vec[i]; //---- O(n)
+    }
 
-   return max_; //---O(1)
+    return max_; //---O(1)
+    }
+    //o(N)
 }
-//o(N)
 
+namespace using_stl
+{
+    int fun(vector<int> &vec)
+    {
+        return *max_element(vec.begin(),vec.end());
+    }
+    //o(N)
+
+}
 
 int main()
 {
     vector<int> vec = {3 , 5 , 6 , 7 , -67};
 
-    cout << fun(vec) << endl;
+    cout << normal::fun(vec) << endl;
+    cout << using_stl::fun(vec) << endl;
     
 }
